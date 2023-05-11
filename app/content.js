bd23baffb65d941cf850734c0673f6c8eaee94af
Solaire <skabrock@gmail.com>
2023-05-11 13:06:30 +0300

    fix: calculate prices or users items

--- app/content.js
+++ app/content.js
@@ -1,27 +1,55 @@
-const exchangeRates = [
-  { name: "PRUB/USD", fragment: "$", rate: 16.35 },
-  // { name: "USD/PRUB", fragment: "$", rate: 16.3 },
-  { name: "PRUB/EUR", fragment: "€", rate: 18.45 },
-  // { name: "EUR/PRUB", fragment: "€", rate: 18.45 },
-  { name: "PRUB/LEI", fragment: "Lei", rate: 0.95 },
-  // { name: "LEI/PRUB", fragment: "Lei", rate: 0.95 },
+const fragments = [
+  { fragment: "$", sign: "USD" },
+  { fragment: "USD", sign: "USD" },
+  { fragment: "€", sign: "EUR" },
+  { fragment: "EUR", sign: "EUR" },
+  { fragment: "руб", sign: "PRUB" },
+  { fragment: "TDRub", sign: "PRUB" },
+  { fragment: "Lei", sign: "LEI" },
 ];
 
-const priceSelector = ".ls-detail_price, .item_title_price";
+const currencySigns = {
+  PRUB: "руб",
+  USD: "$",
+  EUR: "€",
+  LEI: "Lei",
+};
+
+const exchangeRates = [
+  // { name: "PRUB/USD",rate: 16.3 },
+  { name: "USD/PRUB", rate: 16.35 },
+  // { name: "PRUB/EUR", rate: 18.45 },
+  { name: "EUR/PRUB", rate: 18.45 },
+  // { name: "PRUB/LEI", rate: 0.95 },
+  { name: "LEI/PRUB", rate: 0.95 },
+];
+
+const preferredCurrency = "PRUB";
+
+const priceSelector = ".ls-detail_price, .item_title_price, .title>.price";
 
 function updatePrices() {
   document.querySelectorAll(priceSelector).forEach((priceBlock) => {
     priceBlock.setAttribute("data-origin-value", priceBlock.textContent);
 
-    exchangeRates.forEach(({ fragment, rate }) => {
+    fragments.forEach(({ fragment, sign }) => {
       if (priceBlock.textContent.includes(fragment)) {
         const originPrice = Number(priceBlock.textContent.replace(/\D+/g, ""));
+
+        let rate = 1;
+        if (sign !== preferredCurrency) {
+          rate = exchangeRates.find(function ({ name }) {
+            return name === `${sign}/${preferredCurrency}`;
+          }).rate;
+        }
+
         const calculatedPrice = Math.round(originPrice * rate);
 
-        priceBlock.innerHTML = (calculatedPrice + " руб").replace(
-          /(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, // set spaces before every 3 digits
-          "$1" + " "
-        );
+        priceBlock.innerHTML =
+          `${calculatedPrice} ${currencySigns[preferredCurrency]}`.replace(
+            /(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, // set spaces before every 3 digits
+            "$1" + " "
+          );
       }
     });
   });
